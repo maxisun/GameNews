93df4bcccac82f2f61f635aa49183f8dbd04d9f0
maxisun <00354615@uca.edu.sv>
2018-05-31 23:27:45 -0600

    creacion del menu

--- app/src/main/java/com/example/max00/gamenews/Activities/MainActivity.java
+++ app/src/main/java/com/example/max00/gamenews/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.max00.gamenews;
+package com.example.max00.gamenews.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.max00.gamenews.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //commits
+
     }
 }
