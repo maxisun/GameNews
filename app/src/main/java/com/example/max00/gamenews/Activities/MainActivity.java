292b4d01af7afefcb3d94bbfe54a9d359f5b3ae0
maxisun <00354615@uca.edu.sv>
2018-06-01 20:52:29 -0600

    UI del navigation drawer

--- app/src/main/java/com/example/max00/gamenews/Activities/MainActivity.java
+++ app/src/main/java/com/example/max00/gamenews/Activities/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.max00.gamenews.Activities;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.max00.gamenews.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mdrawerLayout;
+    private ActionBarDrawerToggle mactionBarDrawerToggle;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //inicializando atributos
+        initialize();
+        mactionBarDrawerToggle = new ActionBarDrawerToggle(this,mdrawerLayout,R.string.open,R.string.close);
+        mdrawerLayout.addDrawerListener(mactionBarDrawerToggle);
+        //Synchronize the state of the drawer indicator/affordance with the linked DrawerLayout.
+        mactionBarDrawerToggle.syncState();
+        //Set whether home should be displayed as an "up" affordance.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+    }
+    private void initialize(){
+        mdrawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+    }
+
+    //funcion para hacer que funcione el boton para mostrar el drawerlayout
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mactionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
